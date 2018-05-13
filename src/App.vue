003e2ccae3d95161644c5400ae5a71dcfbba0c22
Scott Abbott <scott.abbott.a@gmail.com>
2018-05-13 07:21:04 -0600

    cloned a basic counter app with vue and elm

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Counter  :ports="setupPorts"></Counter>
+
+    <button @click="ports.counter.send(10)">Multiply by 10</button>
+  </div>
+</template>
+
+<script>
+// import Hello from './components/Hello'
+
+import * as ElmComponent from './elm'
+
+export default {
+  name: 'app',
+  components: {
+    'Counter': ElmComponent(require('./Counter.elm').Main)
+  },
+  methods: {
+    setupPorts: function (ports) {
+      ports.watchCounter.subscribe(function (message) {
+        console.log(message)
+      })
+      this.ports = ports
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/Counter.elm
@@ -0,0 +1,70 @@
+-- ./src/Counter.elm
+
+port module Main exposing (..)
+
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+main =
+    Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+    | Multiply Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( model + 1, watchCounter (toString msg) )
+
+        Decrement ->
+            ( model - 1, watchCounter (toString msg) )
+
+        Multiply val ->
+            ( model * val, watchCounter (toString msg) )
+
+
+port counter : (Int -> msg) -> Sub msg
+
+
+port watchCounter : String -> Cmd msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    counter Multiply
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
